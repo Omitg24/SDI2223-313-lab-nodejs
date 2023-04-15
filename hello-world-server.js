b45e43adbb5e36c8dc551cf6758ba9daabd07af5
Omar Teixeira González <91057639+Omitg24@users.noreply.github.com>
2023-04-15 14:52:30 +0200

    SDI-2223-313-10.6-SW Servicio Memoria.

--- hello-world-server.js
+++ hello-world-server.js
@@ -1,6 +1,17 @@
-let http = require('http');
-http.createServer(function handler(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
+let express = require('express');
+let app = express();
+let os = require('os');
+let port = 3000;
+app.get('/memory', function (req, res) {
+    setTimeout(function () { // Espera de 10 segundos
+        console.log(os.freemem());
+        var freeMemory = os.freemem() / 1000000; //pasar a MB
+        res.status(200);
+        res.json({
+            memory: freeMemory
+        });
+    }, 10000);
+});
+app.listen(port, function () {
+    console.log("Servidor listo " + port);
+});
